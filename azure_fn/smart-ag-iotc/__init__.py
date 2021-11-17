ff0b4dfce9f54d1d8f713450213156bf2229a85b
Prashant Subedi <prashantsubedi92@gmail.com>
2021-11-16 23:55:37 -0600

    Configures error reporting with sentry

--- azure_fn/smart-ag-iotc/__init__.py
+++ azure_fn/smart-ag-iotc/__init__.py
@@ -1,12 +1,24 @@
 import logging
+import os
 
 import azure.functions as func
 
+# Sentry for error loggning
+import sentry_sdk
+from sentry_sdk.integrations.serverless import serverless_function
 
+sentry_sdk.init(
+    dsn="https://bdb76d3cccf74020bddc93f372114b96@o1070926.ingest.sentry.io/6067392"
+)
+
+@serverless_function
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
+
+    logging.info(f"{req.get_body()}")
+
     if not name:
         try:
             req_body = req.get_json()
@@ -19,6 +31,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             f"{os.environ} This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
