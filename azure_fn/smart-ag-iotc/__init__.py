05bd0214d64a4ea6caa2472f25e0389ff68de5cf
Prashant Subedi <prashantsubedi92@gmail.com>
2021-12-02 00:38:56 -0600

    Fetch weather data

--- azure_fn/smart-ag-iotc/__init__.py
+++ azure_fn/smart-ag-iotc/__init__.py
@@ -3,21 +3,26 @@ import os
 import json
 
 import azure.functions as func
-
 # Sentry for error loggning
 import sentry_sdk
 from sentry_sdk.integrations.serverless import serverless_function
 
-sentry_dsn = os.getenv("dsn")
+
+from . import weather_api
+
+sentry_dsn = os.getenv("sentry_dsn")
 
 sentry_sdk.init(dsn=sentry_dsn)
 
 @serverless_function
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    payload = json.loads(req.get_body())
+    
+    sensor_data = json.loads(req.get_body())
+    weather_data = weather_api.fetch()
 
-    logging.info(f"{payload}")
+    logging.info(f"sensor_data: {sensor_data}")
+    logging.info(f"weather_data: {weather_api.fetch()}")
 
     name = req.params.get_body(req)
 
