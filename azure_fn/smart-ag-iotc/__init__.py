96838a39f9aeaac69dbd8855605c4e00f7f52a31
Prashant Subedi <prashantsubedi92@gmail.com>
2021-12-03 11:26:22 -0600

    Dump All

--- azure_fn/smart-ag-iotc/__init__.py
+++ azure_fn/smart-ag-iotc/__init__.py
@@ -9,6 +9,7 @@ from sentry_sdk.integrations.serverless import serverless_function
 
 
 from . import weather_api
+from . import device_command
 
 sentry_dsn = os.getenv("sentry_dsn")
 
@@ -18,15 +19,17 @@ sentry_sdk.init(dsn=sentry_dsn)
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     
-    sensor_data = json.loads(req.get_body())
-    weather_data = weather_api.fetch()
+    sensor_data = req.get_json()
+#     weather_data = weather_api.fetch()
+    logging.info(f'{sensor_data}')
+
+    if sensor_data["telemetry"]["moisture"] < 0.6:
+            device_command.start_irrigation()
 
     logging.info(f"sensor_data: {sensor_data}")
     logging.info(f"weather_data: {weather_api.fetch()}")
 
-    name = req.params.get_body(req)
-
     return func.HttpResponse(
-            f"{req.get_body()}",
+            json.dumps(sensor_data),
             status_code=200
     )
