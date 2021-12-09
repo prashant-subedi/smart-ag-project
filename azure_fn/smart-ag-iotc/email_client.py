982fa50589afdb64c5d20e78eae9828cfec2b5bd
Hafiz Oyediran <hoyediran@unomaha.edu>
2021-12-09 14:42:43 -0600

    Pull password from env

--- azure_fn/smart-ag-iotc/email_client.py
+++ azure_fn/smart-ag-iotc/email_client.py
@@ -2,38 +2,43 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import datetime
+import os
 
-#Email Account
-email_sender_account = "hafiz.oyediran@gmail.com" #your email
-email_sender_username = "hafiz.oyediran"  #your email username
-email_sender_password = "ThinkTank21"#your email password
-email_smtp_server = "smtp.gmail.com" #change if not gmail.
-email_smtp_port = 587 #change if needed.
-#Email Content
-email_receivers = ["oyediran.hafiz@gmail.com"]
-email_subject = "Smart IoT Notification"
-email_body = """testing testing testing"""
+EMAIL_SENDER_ACCOUNT = "hafiz.oyediran@gmail.com"
+EMAIL_SENDER_USERNAME = "hafiz.oyediran@gmail.com"
+EMAIL_SENDER_PASSWORD = os.getenv("EMAIL_PASSOWRD")
+EMAIL_SMTP_SERVER = "smtp.gmail.com"
+EMAIL_SMTP_PORT = 587
+
+def setup_server():
+    #Email account settings
+
+    # start connection and login
+    server = smtplib.SMTP(EMAIL_SMTP_SERVER, EMAIL_SMTP_PORT)
+    server.starttls()
+    server.login(EMAIL_SENDER_USERNAME, EMAIL_SENDER_PASSWORD)
+    return server
 
 def send_email(email_subject, email_receivers, email_body):
+    server = setup_server()
     #For loop, sending emails to all email recipients
     for recipient in email_receivers:
         print(f"Sending email to {recipient}")
         message = MIMEMultipart('alternative')
-        message['From'] = email_sender_account
+        message['From'] = EMAIL_SENDER_ACCOUNT
         message['To'] = recipient
         message['Subject'] = email_subject
         message.attach(MIMEText(email_body))
         text = message.as_string()
-        server.sendmail(email_sender_account,recipient,text)
+        server.sendmail(EMAIL_SENDER_ACCOUNT,recipient,text)
     #All emails sent, log out.
     server.quit()
-    
 
-#login to email server
-server = smtplib.SMTP(email_smtp_server,email_smtp_port)
-server.starttls()
-server.login(email_sender_username, email_sender_password)
+if __name__=='__main__':
 
-TimeNow = datetime.datetime.now()
-send_email(email_subject, email_receivers, email_body)
-print(f"End")
+    #Email Content
+    email_receivers = ["oyediran.hafiz@gmail.com, psubedi3@huskers.unl.edu"] #enter the list of recepient emails
+    email_subject = "Smart IoT Notification"
+    email_body = """testing testing testing"""
+
+    send_email(email_subject, email_receivers, email_body)
